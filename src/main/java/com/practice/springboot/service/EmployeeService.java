cab12aeacb363273a85fc51a57c6825f8cb7c2e5
Anamitra Dey <anamitradey.ece@gmail.com>
2021-05-20 12:04:39 -0400

    first commit

---
+++ src/main/java/com/practice/springboot/service/EmployeeService.java
@@ -0,0 +1,15 @@
+package com.practice.springboot.service;
+
+import org.springframework.data.domain.Page;
+
+import com.practice.springboot.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    List<Employee> getAllEmployees();
+    void saveEmployee(Employee employee);
+    Employee getEmployeeById(long id);
+    void deleteEmployeeById(long id);
+    Page<Employee> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection);
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO employees (first_name, last_name, email) VALUES ('Anamitra', 'Dey', 'anamitra.dey@gmail.com');
+INSERT INTO employees (first_name, last_name, email) VALUES ('Sagarika', 'Paul', 'sagarika.paul@gmail.com');
+INSERT INTO employees (first_name, last_name, email) VALUES ('Mira', 'Dey', 'mira.dey@gmail.com');
+INSERT INTO employees (first_name, last_name, email) VALUES ('Kali Pada', 'Dey', 'kalipada.dey@gmail.com');
+INSERT INTO employees (first_name, last_name, email) VALUES ('Avijit', 'Sen', 'avijit.sen@gmail.com');
+INSERT INTO employees (first_name, last_name, email) VALUES ('Sagar', 'Sengupta', 'sagar.sengupta@gmail.com');
+INSERT INTO employees (first_name, last_name, email) VALUES ('Abhishek', 'Dey', 'abhishek.dey@gmail.com');
